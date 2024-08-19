c8ac1d8ced3c6750f04c2279492b4771135d8389
Oscar <moscartong@gmail.com>
2024-08-19 21:48:40 +0800

    init

---
+++ templates/vite/README.md
@@ -0,0 +1,7 @@
+# React + TypeScript + Vite + TailwindCSS
+
+```
+degit m0o0scar/treeee/templates/vite
+```
+
+https://www.npmjs.com/package/degit
---
+++ templates/vite/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="prose-sm max-w-full p-4">
+      <h1>Hello World</h1>
+      <p>This is a Vite + React + TS + Tailwind Template</p>
+    </main>
+  );
+}
