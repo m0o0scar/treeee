21fa1db50a0f6a5f8aeedfbf4623cd8cc14b67d2
Oscar <tangqh@sea.com>
2024-08-21 17:13:39 +0800

    fix: stop using prose-sm, it sets list style as noen

--- templates/next-pages/src/pages/index.tsx
+++ templates/next-pages/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { HelloWorld } from '@/components/HelloWorld';
 
 export default function Home() {
   return (
-    <main className="prose-sm max-w-full p-4">
+    <main className="prose max-w-full p-4">
       <HelloWorld name="Oscar" />
       <p>This is a NextJS + React + TS + Tailwind Template</p>
     </main>
--- templates/vite/src/App.tsx
+++ templates/vite/src/App.tsx
@@ -2,7 +2,7 @@ import { HelloWorld } from './components/HelloWorld';
 
 export default function App() {
   return (
-    <main className="prose-sm max-w-full p-4">
+    <main className="prose max-w-full p-4">
       <HelloWorld />
       <p>This is a Vite + React + TS + Tailwind Template</p>
     </main>
